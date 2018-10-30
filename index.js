4a78d9802aa170b8d70ed91fc74d9a4b2e630215
hristo2612 <hristoapps@gmail.com>
2018-10-30 15:57:57 +0200

    Add index file - this is the crawler

---
+++ index.js
@@ -0,0 +1,16 @@
+var fs = require('fs');
+var path = require('path');
+
+var scrape = require('website-scraper');
+var randomstring = require('randomstring');
+
+var options = {
+  urls: ['http://github.com/hristo2612/'],
+  directory: path.join(__dirname, randomstring.generate(5)) // quick solution for existing directory..
+};
+ 
+scrape(options).then((result) => {
+    console.log(result);
+}).catch((err) => {
+    console.log(err);
+});
